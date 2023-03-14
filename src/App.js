413f5f36753b52f4089130af678790550d6b0d6e
kevinmeans <kevmeans@gmail.com>
2023-03-14 13:52:41 -0700

    Added missing file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,175 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import useSWR from "swr";
+import lookup from "country-code-lookup";
+import axios from 'axios'
+import "./App.scss";
+import "mapbox-gl/dist/mapbox-gl.css";
+mapboxgl.accessToken =
+  "APIKEYACCESSTOKEN";
 
+const fetcher = url => axios.get(url).then(res => res.data)
+var scopeData;
 function App() {
+  const {data,error} = useSWR(
+    '/api/smoke/geojson',
+    fetcher
+  );
+  if (error) console.log("Failed to load");
+  scopeData=data;
+
+  const mapboxElRef = useRef(null);
+      useEffect(() => {
+          const map = new mapboxgl.Map({
+            container: mapboxElRef.current,
+            style: 'mapbox://styles/mapbox/dark-v10',
+            center: [-98, 37], // North America - intial geo location
+            zoom: 3 // initial zoom
+          });
+          map.on('style.load', () => {
+              map.setFog({});
+          });
+          map.on('load', () => {
+
+        map.addSource("earthquakes", {
+          type: "geojson",
+          data:scopeData
+        });
+        map.addLayer(
+           {
+               'id': 'earthquakes-heat',
+               'type': 'heatmap',
+               'source': 'earthquakes',
+               'maxzoom': 9,
+               'paint': {
+                   // Increase the heatmap weight based on frequency and property magnitude
+                   'heatmap-weight': [
+                       'interpolate',
+                       ['linear'],
+                       ['get', 'mag'],
+                       0,
+                       0,
+                       15,
+                       1
+                   ],
+                   // Increase the heatmap color weight weight by zoom level
+                   // heatmap-intensity is a multiplier on top of heatmap-weight
+                   'heatmap-intensity': [
+                       'interpolate',
+                       ['linear'],
+                       ['zoom'],
+                       0,
+                       1,
+                       9,
+                       3
+                   ],
+                   // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+                   // Begin color ramp at 0-stop with a 0-transparancy color
+                   // to create a blur-like effect.
+                   'heatmap-color': [
+                       'interpolate',
+                       ['linear'],
+                       ['heatmap-density'],
+                       0,
+                       'rgba(33,102,172,0)',
+                       0.2,
+                       'rgb(103,169,207)',
+                       0.4,
+                       'rgb(209,229,240)',
+                       0.6,
+                       'rgb(253,219,199)',
+                       0.8,
+                       'rgb(239,138,98)',
+                       1,
+                       'rgb(178,24,43)'
+                   ],
+                   // Adjust the heatmap radius by zoom level
+                   'heatmap-radius': [
+                       'interpolate',
+                       ['linear'],
+                       ['zoom'],
+                       0,
+                       2,
+                       9,
+                      20
+                   ],
+                   // Transition from heatmap to circle layer by zoom level
+                   'heatmap-opacity': [
+                       'interpolate',
+                       ['linear'],
+                       ['zoom'],
+                       7,
+                       1,
+                       9,
+                       0
+                   ]
+               }
+           },
+           'waterway-label'
+       );
+
+
+      map.addLayer(
+            {
+                'id': 'earthquakes-point',
+                'type': 'circle',
+                'source': 'earthquakes',
+                'minzoom': 7,
+                'paint': {
+                    // Size circle radius by earthquake magnitude and zoom level
+                    'circle-radius': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        7,
+                        ['interpolate', ['linear'], ['get', 'mag'], 1, 1, 6, 4],
+                        16,
+                        ['interpolate', ['linear'], ['get', 'mag'], 1, 5, 6, 50]
+                    ],
+                    // Color circle by earthquake magnitude
+                    'circle-color': [
+                        'interpolate',
+                        ['linear'],
+                        ['get', 'mag'],
+                        1,
+                        'rgba(33,102,172,0)',
+                        2,
+                        'rgb(103,169,207)',
+                        3,
+                        'rgb(209,229,240)',
+                        4,
+                        'rgb(253,219,199)',
+                        5,
+                        'rgb(239,138,98)',
+                        6,
+                        'rgb(178,24,43)'
+                    ],
+                    'circle-stroke-color': 'white',
+                    'circle-stroke-width': 1,
+                    // Transition from heatmap to circle layer by zoom level
+                    'circle-opacity': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        7,
+                        0,
+                        8,
+                        1
+                    ]
+                }
+            },
+            'waterway-label'
+        );
+      });
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mapContainer">
+        {/* Mapbox Container */}
+        <div className="mapBox" ref={mapboxElRef} />
+      </div>
     </div>
   );
 }
